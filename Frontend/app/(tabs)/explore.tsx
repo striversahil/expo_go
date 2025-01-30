0ce64267c96d53464e5ac7c187d1e881906cb2cd
Sahil Kumar Jha <striversahil@gmail.com>
2025-01-30 12:48:48 +0530

    Added form handling with react-hook-form and zod; updated tab titles

--- Frontend/app/(tabs)/explore.tsx
+++ Frontend/app/(tabs)/explore.tsx
@@ -1,5 +1,6 @@
 // Explore screen with collapsible sections
-import { StyleSheet, Image, Platform, View, Text } from "react-native";
+
+import { Text, View, Button, StyleSheet } from "react-native";
 
 import { Collapsible } from "@/components/Collapsible";
 import { ExternalLink } from "@/components/ExternalLink";
@@ -9,25 +10,125 @@ import { ThemedView } from "@/components/ThemedView";
 import { IconSymbol } from "@/components/ui/IconSymbol";
 import { HelloWave } from "@/components/HelloWave";
 
-export default function TabTwoScreen() {
+import { useForm, Controller, SubmitHandler } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { TextInput } from "react-native-paper";
+
+export default function TabsScreen() {
+  const userSchema = z.object({
+    username: z
+      .string()
+      .min(5, { message: "Username must be at least 5 characters." })
+      .max(30, { message: "Username must be at most 30 characters." }),
+    email: z.string().email({ message: "Please enter a valid email address." }),
+    age: z.number().min(18, { message: "You must be over 18 years old." }),
+  });
+
+  type UserFormType = z.infer<typeof userSchema>;
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<UserFormType>({
+    resolver: zodResolver(userSchema),
+  });
+
+  const onSubmit: SubmitHandler<UserFormType> = (data: UserFormType) => {
+    console.log(data);
+  };
+
   return (
-    <View style={{ flex: 1 }}>
-      <Text className="text-2xl text-red-600 bg-green-400 rounded-lg p-4 text-center font-bold">
-        Explore
-      </Text>
+    <View style={styles.container} className="bg-white">
+      <Controller
+        control={control}
+        render={({
+          field: { onChange, onBlur, value },
+          fieldState: { error },
+        }) => (
+          <View>
+            <TextInput
+              mode="outlined"
+              label="Username"
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+              activeOutlineColor={error ? "red" : "black"}
+              style={styles.input}
+            />
+          </View>
+        )}
+        name="username"
+      />
+      {errors.username && (
+        <Text style={{ color: "#ff8566" }}>{errors.username.message}</Text>
+      )}
+
+      <Controller
+        control={control}
+        render={({
+          field: { onChange, onBlur, value },
+          fieldState: { error },
+        }) => (
+          <TextInput
+            mode="outlined"
+            label="E-mail"
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+            activeOutlineColor={error ? "red" : "black"}
+            style={styles.input}
+          />
+        )}
+        name="email"
+      />
+      {errors.email && (
+        <Text style={{ color: "#ff8566" }}>{errors.email.message}</Text>
+      )}
+
+      <Controller
+        control={control}
+        render={({
+          field: { onChange, onBlur, value },
+          fieldState: { error },
+        }) => (
+          <TextInput
+            mode="outlined"
+            label="Age"
+            onBlur={onBlur}
+            onChangeText={(text) => {
+              const parsed = parseInt(text, 10);
+              onChange(isNaN(parsed) ? "" : parsed);
+            }}
+            value={
+              value === null || value === undefined ? "" : value.toString()
+            }
+            placeholder="Yaş"
+            keyboardType="numeric"
+            activeOutlineColor={error ? "red" : "black"}
+            style={styles.input}
+          />
+        )}
+        name="age"
+      />
+      {errors.age && (
+        <Text style={{ color: "#ff8566" }}>{errors.age.message}</Text>
+      )}
+
+      <Button title="Submit" onPress={handleSubmit(onSubmit)} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  headerImage: {
-    color: "#808080",
-    bottom: -90,
-    left: -35,
-    position: "absolute",
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    marginVertical: 10,
+    padding: 8,
   },
-  titleContainer: {
-    flexDirection: "row",
-    gap: 8,
+  input: {
+    marginVertical: 10,
   },
 });
